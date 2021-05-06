aa221ccc712eb57c6c3938440fb743d71c751f9d
Lukas Akdogan <lukas.akdogan@gmail.com>
2021-05-06 12:43:39 +0200

    Add files via upload

---
+++ key-gen.py
@@ -0,0 +1,11 @@
+import random
+chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+while 1:
+    key_len = int(input("What length would you like your key to be?: "))
+    key_count = int(input("How many keys would you like?: "))
+    for x in range(0,key_count):
+        key = ""
+        for x in range(0,key_len):
+            key_char = random.choice(chars)
+            key = key + key_char
+        print("Here's your key: ", key)
