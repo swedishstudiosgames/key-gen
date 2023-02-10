3c49bc9fc8be73039ae1c26ab92a0c5e963c4545
Lukas Akdogan <lukas.akdogan@gmail.com>
2023-02-10 10:59:05 +0100

    Update keygen.py

--- keygen.py
+++ keygen.py
@@ -1,1 +1,16 @@
-import random,string def g(k,s,l=16):return s.join(''.join(random.choices(k,k=l//4))for _ in range(4)) keys=[g(string.ascii_lowercase+string.digits,'-')for i in range(int(input("Enter the number of keys you want to generate: ")))]with open("keygen.txt",'w')as file: for key in keys:file.write(key+'\n') print("Keys have been written to the file keygen.txt")
+import os,random,string
+def k(n):
+    k=[];c=string.ascii_uppercase+string.digits
+    for i in range(n):
+        key='-'.join([''.join(random.choices(c,k=4))for i in range(5)])
+        k.append(key)
+    return k
+def f(keys):
+    o=os.name;n="keygen.txt";l=os.getcwd()+"\\"+n if o=='nt'else os.getcwd()+"/"+n
+    with open(l,"w")as file:
+        for key in keys:file.write(key+"\n")
+    return l
+n=int(input("Enter the number of keys you want to generate: "))
+keys=k(n)
+l=f(keys)
+print(f"The product keys are saved in the following file: {l}")
